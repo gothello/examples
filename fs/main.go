22614f225b9e3e1e867feee76e8a6d9102fdb0e1
gothello <marcellosanttos2014@gmail.com>
2023-02-26 15:38:52 -0300

    created example using fsnotify to notify action file is created or removed

--- fs/main.go
+++ fs/main.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"github.com/fsnotify/fsnotify"
+)
+
+func main() {
+	wat, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer wat.Close()
+
+	go func() {
+		for {
+			select {
+			case event, ok := <-wat.Events:
+				if !ok {
+					return
+				}
+				//log.Println("event:", event)
+				switch event.Op {
+				case fsnotify.Create:
+					fmt.Println("created file:", event.Name)
+				case fsnotify.Remove:
+					fmt.Println("removed file:", event.Name)
+				}
+			case err, ok := <-wat.Errors:
+				if !ok {
+					return
+				}
+
+				log.Println("error:", err)
+
+			}
+		}
+	}()
+
+	if err := wat.Add("./"); err != nil {
+		log.Fatalln(err)
+	}
+
+	<-make(chan bool)
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/davecgh/go-spew v1.1.1
+	github.com/fsnotify/fsnotify v1.6.0
 	github.com/google/uuid v1.3.0
 	github.com/redis/go-redis/v9 v9.0.2
 )
@@ -11,4 +12,5 @@ require (
 require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	golang.org/x/sys v0.0.0-20220908164124-27713097b956 // indirect
 )
